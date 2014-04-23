63d92ff150a88a8eced36ca61e173e32660684da
will gossard <pyrobabe696@yahoo.com>
2014-04-23 14:22:23 -0400

    new

--- src/adder.v
+++ src/adder.v
@@ -1,24 +1,25 @@
-/*module   full_adder_32bit( cin, cout, in_a, in_b,  sum
-);
+//
+//module   full_adder_32bit( cin, cout, in_a, in_b,  sum
+//);
     
-    parameter   reg_size = 32;
+ //   parameter   reg_size = 32;
     
-    input   cin;
-    input   [reg_size-1:0] in_a;
-    input   [reg_size-1:0] in_b;
-    output  [reg_size-1:0] sum;
-    output  cout;
+ //   input   cin;
+ //   input   [reg_size-1:0] in_a;
+ //   input   [reg_size-1:0] in_b;
+ //   output  [reg_size-1:0] sum;
+ //   output  cout;
     
-    assign   {cout,sum} = in_a + in_b + cin;
+ //   assign   {cout,sum} = in_a + in_b + cin;
 
-  initial begin
+ // initial begin
  //   sum = 8'hf0;
-    $display("sum : %d", sum);
-end
+//    $display("sum : %d", sum);
+//end
 
-endmodule
+//endmodule
 
-*/
+//
 
 //-----------------------------------------------------
 // This is simple adder Program
