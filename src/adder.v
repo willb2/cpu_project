4a99f556b94dc0e0db2cda9ce5e96545445fc5f3
will gossard <pyrobabe696@yahoo.com>
2014-04-23 14:04:45 -0400

    new adder

--- src/adder.v
+++ src/adder.v
@@ -10,10 +10,10 @@ module   full_adder_32bit( cin, cout, in_a, in_b,  sum
     output  cout;
     
     assign   {cout,sum} = in_a + in_b + cin;
-    
+
   initial begin
-    acc = 8'hf0;
-    $display("acc : %d", acc);
+    sum = 8'hf0;
+    $display("sum : %d", sum);
 end
 
 endmodule
