4973c468042b21bd10dd38b23519ec1e29779791
will gossard <pyrobabe696@yahoo.com>
2014-04-23 13:32:32 -0400

    adder input change
    cin cout, reg 1 reg 2, sum

--- src/adder.v
+++ src/adder.v
@@ -1,10 +1,14 @@
-
-
-module adder(data1, data2, result);
-	input [31:0] data1, data2;
-	output [31:0] result;
+module   full_adder_4bit( cin, cout, in_a, in_b,  sum
+);
     
-
+    parameter   reg_size = 4;
     
-
+    input   cin;
+    input   [reg_size-1:0] in_a;
+    input   [reg_size-1:0] in_b;
+    output  [reg_size-1:0] sum;
+    output  cout;
+    
+    assign   {cout,sum} = in_a + in_b + cin;
+    
 endmodule
